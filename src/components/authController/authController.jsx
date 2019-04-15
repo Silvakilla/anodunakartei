634b8e0b360ca219b7e462d3f75f1bc97f27850f
Silvakilla <goldkilla34@gmail.com>
2019-04-15 22:28:35 +0200

    login

--- src/components/authController/authController.jsx
+++ src/components/authController/authController.jsx
@@ -62,11 +62,14 @@ export default class AuthController extends Component {
 
     decrypt = (payload) => {
         let object = JSON.parse(payload);
-        let val1 = Buffer.from(object.val1, 'hex');
-        let val2 = Buffer.from(object.val2, 'hex');
-        let decipher = crypto.createDecipheriv('aes-256-gcm', Buffer.from(config.cryptoData.secret), val1);
-        let decrypted = decipher.update(val2, 'hex', 'utf8');
-        return JSON.parse(decrypted);
+
+        if(object !== null) {
+            let val1 = Buffer.from(object.val1, 'hex');
+            let val2 = Buffer.from(object.val2, 'hex');
+            let decipher = crypto.createDecipheriv('aes-256-gcm', Buffer.from(config.cryptoData.secret), val1);
+            let decrypted = decipher.update(val2, 'hex', 'utf8');
+            return JSON.parse(decrypted);
+        }
     }
 
     state = {
