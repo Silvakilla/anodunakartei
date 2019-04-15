a816df698c859bcad8bbeadf1600cad8b26649db
Silvakilla <goldkilla34@gmail.com>
2019-04-15 21:05:07 +0200

    login

--- src/components/authController/authController.jsx
+++ src/components/authController/authController.jsx
@@ -5,7 +5,6 @@ import axios from 'axios';
 
 import { GenerateSessionToken } from '../../utils/tokenGenerator';
 import config from '../../../config/config';
-import bcrypt from 'bcrypt';
 
 const AuthContext = createContext({});
 
